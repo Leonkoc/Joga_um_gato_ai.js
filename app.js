c4847690beedc8e3d5e0836ecbdfc108c5a0b30c
Leonkoc <leonkocc@hotmail.com>
2022-06-26 12:58:41 -0300

    getcatjs

---
+++ app.js
@@ -0,0 +1,28 @@
+const BASE_URL = "https://api.thecatapi.com/v1/images/search";
+
+const catBtn = document.getElementById("change-cat");
+
+// const catImg = document.getElementById('cat')
+
+const getCats = async () => {
+  const data = await fetch(BASE_URL, {
+    mode: "cors", //Ativar modo cors nessa configuração
+  })
+    .then((res) => res.json())
+
+    .then((json) => json[0].url) //[0] pega a primeira imagem // .url em vez de .weburl
+
+    .catch((error) => console.log(error));
+
+  return data;
+};
+
+const loadImg = async () => {
+  const catImg = document.getElementById("cat");
+
+  catImg.src = await getCats();
+};
+
+catBtn.addEventListener("click", loadImg);
+
+loadImg();
